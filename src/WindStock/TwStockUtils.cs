4485a60a9ce617bb766c8ca3300fe44b0ba7db30
TingYu_Wu <tingyu_wu@cmoney.com.tw>
2024-03-29 13:36:49 +0800

    add [feature] IsClose method.

--- src/WindStock/TwStockUtils.cs
+++ src/WindStock/TwStockUtils.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -6,6 +7,22 @@ namespace WindStock
 {
     internal static class TwStockUtils
     {
+        public static bool IsClose()
+        {
+            var now = DateTime.Now;
+            if (now.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday)
+            {
+                return true;
+            }
+
+            if (now is { Hour: >= 13, Minute: > 30 } or { Hour: < 8 })
+            {
+                return true;
+            }
+
+            return false;
+        }
+
         public static IEnumerable<TwseStockInfo> GetAllTwse()
         {
             var csvPath = Path.Combine(Directory.GetCurrentDirectory(), "Csv", "all_twse.csv");
